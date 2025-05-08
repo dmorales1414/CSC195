37766e9d1639ce88e9ed1aabf63e1d6bf1840676
dmorales1414 <dmorales@student.neumont.edu>
2025-05-07 20:09:10 -0600

    Added Git message to Main.cpp

---
+++ CSC195/Main/Main.cpp
@@ -0,0 +1,15 @@
+// Main.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "Git is now tracking this project!" << endl;
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
